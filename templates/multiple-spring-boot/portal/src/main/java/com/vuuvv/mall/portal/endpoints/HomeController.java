6bbbfacc673a4270b1baeaadd29247bd41d5efa1
vuuvv <vuuvv@qq.com>
2019-08-27 23:20:00 +0800

    spring boot multiple module application

---
+++ templates/multiple-spring-boot/portal/src/main/java/com/vuuvv/mall/portal/endpoints/HomeController.java
@@ -0,0 +1,17 @@
+package com.vuuvv.mall.portal.endpoints;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by vuuvv on 2019-08-23
+ */
+@RequestMapping("")
+@RestController
+public class HomeController {
+    @GetMapping("")
+    public Object hello() {
+        return "Hello World!";
+    }
+}
