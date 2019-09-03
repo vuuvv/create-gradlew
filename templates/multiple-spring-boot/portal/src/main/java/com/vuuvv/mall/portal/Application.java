6e97c2abe3575b76a819d11a00f6024bcaded8b7
vuuvv <vuuvv@qq.com>
2019-09-03 17:29:10 +0800

    update mutiple spring boot template

--- templates/multiple-spring-boot/portal/src/main/java/com/vuuvv/mall/portal/Application.java
+++ templates/multiple-spring-boot/portal/src/main/java/com/vuuvv/mall/portal/Application.java
@@ -1,9 +1,13 @@
 package com.vuuvv.mall.portal;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication()
 public class Application {
 
     public static void main(String[] args) {
